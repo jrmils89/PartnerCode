ad0c18608f4fc8fdb120f1fc71c95fe9f0b063ef
Jesse Mills <jessemills1989@gmail.com>
2016-03-30 13:38:27 -0400

    Remove scroll actions on mobile

--- public/js/controllers/homeController.js
+++ public/js/controllers/homeController.js
@@ -7,19 +7,21 @@ app.controller("homeController", [function() {
 
   this.hello = "Hello";
 
-  window.addEventListener("scroll", function(e) {
-    if (window.scrollY >= 380) {
-      document.getElementsByClassName("intro-container")[0].className = document.getElementsByClassName("intro-container")[0].className.replace(/(intro\-container)/gi,'hidden');
-      window.scroll(0,10);
-    }
-    if (window.scrollY < 5 && document.getElementsByClassName("hidden") != []) {
-
-      var lastEl = document.getElementsByClassName("hidden").length - 1;
-      if (lastEl != -1) {
-        document.getElementsByClassName('hidden')[lastEl].className = document.getElementsByClassName('hidden')[lastEl].className.replace(/(hidden)/gi,'intro-container');
-        window.scroll(0,379);
+  if( !/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+    window.addEventListener("scroll", function(e) {
+      if (window.scrollY >= 380) {
+        document.getElementsByClassName("intro-container")[0].className = document.getElementsByClassName("intro-container")[0].className.replace(/(intro\-container)/gi,'hidden');
+        window.scroll(0,10);
       }
-    }
-  })
+      if (window.scrollY < 5 && document.getElementsByClassName("hidden") != []) {
+
+        var lastEl = document.getElementsByClassName("hidden").length - 1;
+        if (lastEl != -1) {
+          document.getElementsByClassName('hidden')[lastEl].className = document.getElementsByClassName('hidden')[lastEl].className.replace(/(hidden)/gi,'intro-container');
+          window.scroll(0,379);
+        }
+      }
+    })
+  };
 
 }]);
