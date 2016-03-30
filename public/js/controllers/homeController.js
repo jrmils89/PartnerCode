9e99139008844158e0f93937d1d0ebc79faba5b8
Jesse Mills <jessemills1989@gmail.com>
2016-03-30 13:33:38 -0400

    Tweaking styling and scroll actions to improve mobile

--- public/js/controllers/homeController.js
+++ public/js/controllers/homeController.js
@@ -8,16 +8,16 @@ app.controller("homeController", [function() {
   this.hello = "Hello";
 
   window.addEventListener("scroll", function(e) {
-    if (window.scrollY >= 400) {
+    if (window.scrollY >= 380) {
       document.getElementsByClassName("intro-container")[0].className = document.getElementsByClassName("intro-container")[0].className.replace(/(intro\-container)/gi,'hidden');
-      window.scroll(0,100);
+      window.scroll(0,10);
     }
     if (window.scrollY < 5 && document.getElementsByClassName("hidden") != []) {
 
       var lastEl = document.getElementsByClassName("hidden").length - 1;
       if (lastEl != -1) {
         document.getElementsByClassName('hidden')[lastEl].className = document.getElementsByClassName('hidden')[lastEl].className.replace(/(hidden)/gi,'intro-container');
-        window.scroll(0,380);
+        window.scroll(0,379);
       }
     }
   })
