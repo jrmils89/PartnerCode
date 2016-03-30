7f778be8659a0999bf04f21d3cbaf6c07331904a
Jesse Mills <jessemills1989@gmail.com>
2016-03-30 13:45:06 -0400

    Updating mobile check based on Mozilla rec

--- public/js/controllers/homeController.js
+++ public/js/controllers/homeController.js
@@ -7,7 +7,7 @@ app.controller("homeController", [function() {
 
   this.hello = "Hello";
 
-  if( !/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+  if( !/Mobi/i.test(navigator.userAgent) ) {
     window.addEventListener("scroll", function(e) {
       if (window.scrollY >= 380) {
         document.getElementsByClassName("intro-container")[0].className = document.getElementsByClassName("intro-container")[0].className.replace(/(intro\-container)/gi,'hidden');
@@ -19,9 +19,9 @@ app.controller("homeController", [function() {
         if (lastEl != -1) {
           document.getElementsByClassName('hidden')[lastEl].className = document.getElementsByClassName('hidden')[lastEl].className.replace(/(hidden)/gi,'intro-container');
           window.scroll(0,379);
-        }
-      }
-    })
+        };
+      };
+    });
   };
 
 }]);
